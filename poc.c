ec629b2b417980964de257a8200696067287431f
kathirvelchitra <31887330+kathirvelchitra@users.noreply.github.com>
2018-01-19 08:31:36 +0000

    Create poc.c

---
+++ poc.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter the value");
+    scanf("%d",&a);
+    if(a<0)
+    {
+    printf("negative");
+    }
+    else
+    {
+    printf("positive");
+}
+
+    return 0;
+}
+
